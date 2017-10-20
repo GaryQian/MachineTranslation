6c5ba164c2c44f96ff2688c8389186fb204b7050
Gary Qian <gqian1@jhu.edu>
2017-10-19 20:11:34 -0400

    N list stuff and hyper greedy

--- decoder/move.py
+++ decoder/move.py
@@ -12,14 +12,15 @@ def move(sentence=list):
 
         cureval = sentence.index(frlist[i])
         nexteval = sentence.index(frlist[i+1])
-
         if abs(cureval - nexteval) > 2:
             bigger = max(cureval,nexteval)
-            temp1 = copy.deepcopy(sentence)
-            temp2 = copy.deepcopy(sentence)
-            temp1[bigger-2],temp1[bigger] = sentence[bigger], sentence[bigger-2]
-            temp1[bigger-1],temp2[bigger] = sentence[bigger], sentence[bigger-1]
-            
+            temp1 = copy.copy(sentence)
+            temp2 = copy.copy(sentence)
+            #print cureval
+            #print nexteval
+            temp1[bigger-2],temp1[bigger] = temp1[bigger], temp1[bigger-2]
+            temp2[bigger-1],temp2[bigger] = sentence[bigger], sentence[bigger-1]
+            #print str(temp1) + " " + str(temp2)
             greedyH.append(temp1)
             greedyH.append(temp2)
     return greedyH
